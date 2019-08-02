453ad5359ff25e8367812a231a74d7de80ce8ded
chyciu <jarek.chwyc@yahoo.com>
2019-08-02 15:15:26 +0200

    pętle

--- src/main/java/petle/SumaCiaguLiczb.java
+++ src/main/java/petle/SumaCiaguLiczb.java
@@ -9,12 +9,14 @@ public class SumaCiaguLiczb {
 
         SumaCiaguLiczb sumaCiaguLiczb = new SumaCiaguLiczb();
         sumaCiaguLiczb.ciagLiczb();
+
     }
 
 
     public void ciagLiczb() {
 
         try {
+
             Scanner scanner = new Scanner(System.in);
             System.out.println("Podaj liczbę całkowitą A.");
             int liczbaA = scanner.nextInt();
@@ -23,20 +25,38 @@ public class SumaCiaguLiczb {
             System.out.println("Liczba A: " + liczbaA);
             System.out.println("Liczba B: " + liczbaB);
 
-            if (liczbaA<liczbaB) {
-
+            if (liczbaA < liczbaB) {
                 int sum = IntStream.rangeClosed(liczbaA, liczbaB).sum();
                 System.out.println("Suma ciągu: " + sum);
             } else {
-                System.out.println("Liczba A musi być mniejsza od liczby B.");
+                System.out.println("Liczba A musi być większa od liczby B.");
                 ciagLiczb();
             }
+
+            int suma = 0;
+            for (int i = liczbaA; i <= liczbaB; i++) {
+                suma = suma+=i;
+            }
+            System.out.println("Suma ciągu z petlą -for-: " + suma);
+
+            int newSuma = 0;
+            do {
+
+                newSuma = newSuma += liczbaA;
+                liczbaA++;
+
+            } while (liczbaA <= liczbaB);
+            System.out.println("Suma ciągu z pętlą - do - while -: " + newSuma);
+
+
+
         } catch (Exception e) {
             System.out.println("Nie podałeś liczb.");
             ciagLiczb();
         }
     }
 }
+
 
 
 //    Napisac program pobierajacy od uzytkownika dwie liczby całkowite A oraz B,
